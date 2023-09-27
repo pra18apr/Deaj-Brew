59ea0b897882619fe953360de90cc3c8aeef48e3
pra18apr <pra.18apr@gmail.com>
2023-09-27 17:16:02 -0600

    added categorymenu files to project

---
+++ client/src/components/CategoryMenu/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  buttons: {
+    margin: '10px',
+    backgroundColor: '#4a2c2a',
+    color: 'white',
+  },
+}));
