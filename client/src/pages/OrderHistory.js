677d5a78424150514cecbddc576a3c6383f96a93
pra18apr <pra.18apr@gmail.com>
2023-09-27 17:30:12 -0600

    added Orderhistory.js files to project

---
+++ client/src/pages/OrderHistory.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
+
+function OrderHistory() {
+  const { data } = useQuery(QUERY_USER);
+  let user;
+
+  if (data) {
+    user = data.user;
+  }
+
+  return (
+    <>
+      <div className="container my-1">
+        <Link to="/menu">← Back to Products</Link>
+
+        {user ? (
+          <>
+            <h3 className= "mx-3">
+              Order History for {user.firstName} {user.lastName}
+            </h3>
+            {user.orders.map((order) => (
+              <div key={order._id} className="my-2">
+                <h3 className= "mx-3">
+                  {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
+                </h3>
+                <div className="flex-row">
+                  {order.products.map(({ _id, image, name, price }, index) => (
+                    <div key={index} className="card px-1 py-1">
+                      <Link to={`/products/${_id}`}>
+                        <img alt={name} src={`/images/${image}`} />
+                        <p>{name}</p>
+                      </Link>
+                      <div>
+                        <span>${price}</span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </>
+        ) : null}
+      </div>
+    </>
+  );
+}
+
+export default OrderHistory;
